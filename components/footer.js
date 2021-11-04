f2a7744d9e16ca58578724d789e0cf73b3ceb949
Guilherme <guilherme.dev2019@gmail.com>
2021-11-04 12:41:33 -0400

    Adicionado estrutura basica e componentes header e footer

---
+++ components/footer.js
@@ -0,0 +1,48 @@
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.innerHTML = `
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" />
+    <div class="container-fluid bg-warning bg-gradient">
+      <footer class="px-5 pt-5">
+        <div class="row">
+          <div class="col-8">
+            <img src="../assets/logo.svg" style="width:150px;" class="mb-3"/>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2"><a href="/index.html" class="nav-link p-0 text-muted">Home</a></li>
+              <li class="nav-item mb-2"><a href="/pages/ovopedia.html" class="nav-link p-0 text-muted">Ovopedia</a></li>
+              <li class="nav-item mb-2"><a href="/pages/quemSomos.html" class="nav-link p-0 text-muted">Quem Somos</a></li>
+              <li class="nav-item mb-2"><a href="/pages/faleConosco.html" class="nav-link p-0 text-muted">Fale Conosco</a></li>
+            </ul>
+          </div>
+    
+          <div class="col-4">
+            <form method="get" action="../file/LANCHES-RÁPIDOS-E-SAUDÁVEIS.pdf" target="_blank">
+              <h5>Coloque seu email para baixar um ebook grátis!</h5>
+              <p>Ebook com receitas e informações nutricionais.</p>
+                <div class="d-flex w-100 gap-2">
+                  <label for="newsletter1" class="visually-hidden">Email address</label>
+                  <input required id="newsletter1" type="email" class="form-control" placeholder="Endereço de Email">
+                  <button type="submit" class="btn btn-primary" type="button">Baixar</button>
+                </div>
+            </form>
+          </div>
+        </div>
+    
+        <div class="d-flex justify-content-between py-4 mt-4 border-top">
+          <p>© 2021 Ovolivery, Inc. Todos os direitos reservados.</p>
+          <ul class="list-unstyled d-flex">
+            <li class="ms-3"><a class="link-dark" href="#"><img src="https://img.icons8.com/ios-glyphs/30/000000/facebook.png"/></a></li>
+            <li class="ms-3"><a class="link-dark" href="#"><img src="https://img.icons8.com/ios-glyphs/30/000000/instagram-new.png"/></a></li>
+            <li class="ms-3"><a class="link-dark" href="#"><img src="https://img.icons8.com/ios-glyphs/30/000000/twitter--v1.png"/></a></li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+    `
+  }
+}
+
+window.customElements.define("footer-container", Footer);
---
+++ components/header.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  let nav = `
+  <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-warning bg-gradient p-md-5 p-sm-4 p-3 fs-4">
+    <div class="container-fluid px-xl-5">
+      <a class="navbar-brand mx-0" href="/index.html"><img src="../assets/logo.png" style="width:180px;"/></a>
+      
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav mx-auto">
+          <a class="nav-link active" aria-current="page" href="/index.html">Home</a>
+          <a class="nav-link" href="/pages/ovopedia.html">Ovopedia</a>
+          <a class="nav-link" href="/pages/quemSomos.html">Quem Somos</a>
+          <a class="nav-link" href="/pages/faleConosco.html">Fale Conosco</a>
+        </div>
+        <div class="navbar-nav">
+          <a class="nav-link" href="/pages/carrinho.html"><img src="../assets/carrinho.svg"/></a>
+        </div>
+      </div>
+    </div>
+  </nav>
+`
+
+  $('header').append(nav)
+  $('a.active').removeClass('active');
+  $('a[href="' + location.pathname + '"]').addClass('active');
+})
+
+
+
